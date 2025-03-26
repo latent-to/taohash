b28a8e2de22ada2257885c09182448c602531243
Cameron Fairchild <cameron@opentensor.ai>
2025-03-25 22:16:12 -0400

    fix braiins pool api

--- taohash/pool/braiins/api.py
+++ taohash/pool/braiins/api.py
@@ -9,29 +9,31 @@ from ..api import PoolAPI
 
 HASHRATE_TTL = 10 * 60  # 10min TTL for grabbing hashrate from all workers
 
-__UNIT_TO_GHS: Dict[str, float] = {
-    "H/s": 1e-9,
-    "Kh/s": 1e-6,
-    "Mh/s": 1e-3,
-    "Gh/s": 1.0,
-    "Th/s": 1e3,
-    "Ph/s": 1e6,
-}
-
-__COIN_TO_COIN_NAME = {
-    "bitcoin": "btc",
-}
-
 
 class BraiinsPoolAPI(PoolAPI):
+    __UNIT_TO_GHS: Dict[str, float] = {
+        "H/s": 1e-9,
+        "Kh/s": 1e-6,
+        "Mh/s": 1e-3,
+        "Gh/s": 1.0,
+        "Th/s": 1e3,
+        "Ph/s": 1e6,
+    }
+
+    __COIN_TO_COIN_NAME = {
+        "bitcoin": "btc",
+    }
+
     """
     An API for the Braiins pool monitoring
     See: https://academy.braiins.com/en/braiins-pool/monitoring/#overview
     """
 
-    def _hashrate_to_gh(hashrate: float, unit: str) -> float:
-        return __UNIT_TO_GHS[unit] * hashrate
+    @classmethod
+    def _hashrate_to_gh(cls, hashrate: float, unit: str) -> float:
+        return cls.__UNIT_TO_GHS[unit] * hashrate
 
+    @staticmethod
     def _worker_name_to_worker_id(worker_name: str) -> str:
         return worker_name.split(".", maxsplit=1)[1]
 
@@ -39,7 +41,7 @@ class BraiinsPoolAPI(PoolAPI):
     @on_exception(expo, RateLimitException, max_tries=8)
     @limits(calls=1, period=5)  # rate limit once per 5s
     def _get_shares_for_workers(self, coin: str) -> Dict[str, float]:
-        coin_name = __COIN_TO_COIN_NAME[coin]
+        coin_name = self.__COIN_TO_COIN_NAME[coin]
         url = f"https://pool.braiins.com/accounts/workers/json/{coin_name}"
 
         response = requests.get(
@@ -54,10 +56,8 @@ class BraiinsPoolAPI(PoolAPI):
         workers = result[coin_name]["workers"]
 
         output = {
-            self._worker_name_to_worker_id(worker_name): self._hashrate_to_gh(
-                worker_data["shares_5m"]
-            )
-            for worker_name, worker_data in workers
+            self._worker_name_to_worker_id(worker_name): worker_data["shares_5m"]
+            for worker_name, worker_data in workers.items()
         }
 
         return output
@@ -74,7 +74,7 @@ class BraiinsPoolAPI(PoolAPI):
         if coin != "bitcoin":
             raise ValueError("BraiinsPool only supports bitcoin")
 
-        coin_name = __COIN_TO_COIN_NAME[coin]
+        coin_name = self.__COIN_TO_COIN_NAME[coin]
         url = f"https://pool.braiins.com/stats/json/{coin_name}"
 
         response = requests.get(
