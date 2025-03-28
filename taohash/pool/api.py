a5f96aebc8576c9c2e8756825d9a765ffa886f09
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-03-27 17:13:23 -0700

    update pool/api

--- taohash/pool/api.py
+++ taohash/pool/api.py
@@ -1,16 +1,19 @@
 import abc
 
-
-class PoolAPI(metaclass=abc.ABCMeta):
-    api_key: str
-
+class PoolAPI(abc.ABC):
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
     @abc.abstractmethod
-    def get_shares_for_worker(worker_id: str) -> float:
-        pass
+    def get_shares_for_worker(self, worker_id: str, coin: str = "bitcoin") -> float:
+        """
+        Retrieve the number of shares for a given worker.
+        """
+        raise NotImplementedError("Subclasses must implement get_shares_for_worker")
 
     @abc.abstractmethod
-    def get_fpps(self, coin: str) -> float:
-        pass
+    def get_fpps(self, coin: str = "bitcoin") -> float:
+        """
+        Retrieve the fee-per-share (FPPS) rate for the given coin.
+        """
+        raise NotImplementedError("Subclasses must implement get_fpps")
