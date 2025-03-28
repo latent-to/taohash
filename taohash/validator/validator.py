29fdb95418dd895611edc2f0641ef6693776551a
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-03-27 17:22:59 -0700

    Updates validator

--- taohash/validator/validator.py
+++ taohash/validator/validator.py
@@ -1,51 +1,52 @@
+import argparse
+import os
+import socket
+import traceback
+import time
 from typing import List, Optional
 
-import os
-import random
-import argparse
-import traceback
 import bittensor as bt
-
-from node import Node
-from pool import Pool, PoolIndex
-import utils
-from pool.metrics import get_metrics_for_miners, MiningMetrics
-from ..pricing import CoinPriceAPI, CoinPriceAPIBase
+from taohash.pool import Pool, BraiinsPool
+from taohash.pool.metrics import MiningMetrics, get_metrics_for_miners
+from taohash.pricing import CoinPriceAPI, PriceAPIMethod
 
 
 class Validator:
     def __init__(self):
         self.config = self.get_config()
         self.setup_logging()
+        self.pool = Pool(
+            pool=self.config.pool.pool,
+            api_key=self.config.pool.api_key,
+            config=self.config,
+        )
+        self.price_api: PriceAPIMethod = CoinPriceAPI(
+            method=self.config.price.method, api_key=self.config.price.api_key
+        )
         self.setup_bittensor_objects()
+        self._set_pool_commitment()
         self.last_update = 0
         self.my_uid = self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)
         self.scores = [1.0] * len(self.metagraph.S)
         self.last_update = 0
         self.current_block = 0
-        self.tempo = self.node_query("SubtensorModule", "Tempo", [self.config.netuid])
+        self.tempo = self.subtensor.tempo(self.config.netuid)
         self.moving_avg_scores = [1.0] * len(self.metagraph.S)
         self.alpha = 0.1
-        self.node = Node(url=self.config.subtensor.chain_endpoint)
-        self.pool = Pool(pool=self.config.pool.pool, api_key=self.config.pool.api_key)
-        self.price_api: CoinPriceAPIBase = CoinPriceAPI(
-            method=self.config.price.method, api_key=self.config.price.api_key
+        self.last_update = self.subtensor.blocks_since_last_update(
+            self.config.netuid, self.my_uid
         )
 
     def get_config(self):
-        # Set up the configuration parser.
         parser = argparse.ArgumentParser()
-        # TODO: Add your custom validator arguments to the parser.
         parser.add_argument(
             "--custom",
             default="my_custom_value",
             help="Adds a custom value to the parser.",
         )
-        # Adds override arguments for network and netuid.
         parser.add_argument(
             "--netuid", type=int, default=1, help="The chain subnet uid."
         )
-
         parser.add_argument(
             "--coins",
             type=str,
@@ -53,19 +54,13 @@ class Validator:
             default=["bitcoin"],
             help="The coins you wish to reward miners for. Use CoinGecko token naming",
         )
-        # Adds subtensor specific arguments.
         bt.subtensor.add_args(parser)
-        # Adds logging specific arguments.
         bt.logging.add_args(parser)
-        # Adds wallet specific arguments.
         bt.wallet.add_args(parser)
-
         Pool.add_args(parser)
         CoinPriceAPI.add_args(parser)
 
-        # Parse the config.
         config = bt.config(parser)
-        # Set up logging directory.
         config.full_path = os.path.expanduser(
             "{}/{}/{}/netuid{}/{}".format(
                 config.logging.logging_dir,
@@ -75,12 +70,10 @@ class Validator:
                 "validator",
             )
         )
-        # Ensure the logging directory exists.
         os.makedirs(config.full_path, exist_ok=True)
         return config
 
     def setup_logging(self):
-        # Set up logging.
         bt.logging(config=self.config, logging_dir=self.config.full_path)
         bt.logging.info(
             f"Running validator for subnet: {self.config.netuid} on network: {self.config.subtensor.network} with config:"
@@ -88,92 +81,87 @@ class Validator:
         bt.logging.info(self.config)
 
     def setup_bittensor_objects(self):
-        # Build Bittensor validator objects.
         bt.logging.info("Setting up Bittensor objects.")
 
-        # Initialize wallet.
         self.wallet = bt.wallet(config=self.config)
         bt.logging.info(f"Wallet: {self.wallet}")
 
-        # Initialize subtensor.
         self.subtensor = bt.subtensor(config=self.config)
         bt.logging.info(f"Subtensor: {self.subtensor}")
 
-        # Initialize metagraph.
         self.metagraph = self.subtensor.metagraph(self.config.netuid)
         bt.logging.info(f"Metagraph: {self.metagraph}")
 
-        # Connect the validator to the network.
         if self.wallet.hotkey.ss58_address not in self.metagraph.hotkeys:
             bt.logging.error(
                 f"\nYour validator: {self.wallet} is not registered to chain connection: {self.subtensor} \nRun 'btcli register' and try again."
             )
             exit()
         else:
-            # Each validator gets a unique identity (UID) in the network.
             self.my_subnet_uid = self.metagraph.hotkeys.index(
                 self.wallet.hotkey.ss58_address
             )
             bt.logging.info(f"Running validator on uid: {self.my_subnet_uid}")
 
-        # Set up initial scoring weights for validation.
+        # Initial weights scoring
         bt.logging.info("Building validation weights.")
         self.scores = [1.0] * len(self.metagraph.S)
         bt.logging.info(f"Weights: {self.scores}")
 
-        # Set an axon of the pool address
         self._serve_axon(
             self.wallet,
             port=self.pool.port,
             ip=self.pool.ip,
-            pool_index=self.pool.index,
         )
+
+    def _set_pool_commitment(self) -> None:
+        """Set the pool commitment for Braiins pool."""
+        if isinstance(self.pool, BraiinsPool):
+            data = f"{self.pool.index.value}:{self.config.pool.user_id}:{self.config.pool.password}"
+        else:
+            data = f"{self.pool.index.value}:"  # for now
+
+        success = self.subtensor.commit(
+            wallet=self.wallet, netuid=self.config.netuid, data=data
+        )
+        if not success:
+            raise ValueError("Failed to set pool commitment")
 
     def _serve_axon(
-        self, wallet: bt.wallet, ip: str, port: int, pool_index: PoolIndex
+        self,
+        wallet: bt.wallet,
+        ip: str,
+        port: int,
     ) -> None:
-        wallet.hotkey
-        params = {
-            "version": bt.__version_as_int__,
-            "ip": utils.ip_to_int(ip),
-            "port": port,
-            "ip_type": utils.ip_version(ip),
-            "netuid": self.config.netuid,
-            "hotkey": wallet.hotkey.ss58_address,
-            "coldkey": wallet.coldkeypub.ss58_address,
-            "protocol": pool_index.value,  # Use protocol for setting the pool
-            "placeholder1": 0,
-            "placeholder2": 0,
-        }
+        # Check if we're already serving this pool
         uid = self.metagraph.hotkeys.index(wallet.hotkey.ss58_address)
         current_axon = self.metagraph.axons[uid]
-        if current_axon.protocol == params["protocol"]:
-            return  # same pool, don't serve twice
 
-        axon_call = self.node.compose_call(
-            call_module="SubtensorModule",
-            call_function="serve_axon",
-            call_params=params,
+        try:
+            if not ip.replace(".", "").isdigit():  # Check if ip is a domain name
+                ip = socket.gethostbyname(ip)
+        except socket.gaierror as e:
+            raise RuntimeError(f"Could not resolve pool address: {ip}. Error: {str(e)}")
+
+        if current_axon.ip == ip and current_axon.port == port:
+            return  # same axon, don't serve twice
+
+        axon = bt.axon(
+            wallet=wallet,
+            external_ip=ip,  # Use pool IP as external IP
+            external_port=port,  # Use pool port for external port
         )
-        extrinsic = self.node.create_signed_extrinsic(
-            call=axon_call, keypair=wallet.hotkey
-        )
-        response = self.node.submit_extrinsic(
-            extrinsic,
+        bt.logging.info(f"Serving axon: {axon}")
+        success = self.subtensor.serve_axon(
+            axon=axon,
+            netuid=self.config.netuid,
             wait_for_inclusion=True,
             wait_for_finalization=True,
         )
-        response.process_events()
-        if not response.is_success:
+        if not success:
             raise RuntimeError("Pool address could not be served")
 
-    def node_query(self, module, method, params):
-        result = self.node.query(module, method, params).value
-
-        return result
-
     def run(self):
-        # The Main Validation Loop.
         bt.logging.info("Starting validator loop.")
         while True:
             try:
@@ -181,9 +169,9 @@ class Validator:
                 hotkey_to_uid = {n.hotkey: n.uid for n in self.metagraph.neurons}
 
                 for coin in self.config.coins:
-                    miner_metrics: List[MiningMetrics] = [
-                        get_metrics_for_miners(self.pool, self.metagraph.neurons)
-                    ]
+                    miner_metrics: List[MiningMetrics] = get_metrics_for_miners(
+                        self.pool, self.metagraph.neurons
+                    )
                     coin_price: Optional[float] = self.price_api.get_price(coin)
                     if coin_price is None:
                         # If we can't grab the price, don't count the shares
@@ -194,7 +182,7 @@ class Validator:
                     for metric in miner_metrics:
                         uid = hotkey_to_uid[metric.hotkey]
 
-                        shares_value: float = MiningMetrics.get_shares_value(fpps)
+                        shares_value: float = metric.get_shares_value(fpps)
                         in_usd: float = shares_value * coin_price
 
                         current_scores[uid] += in_usd
@@ -206,27 +194,23 @@ class Validator:
 
                 bt.logging.info(f"Moving Average Scores: {self.moving_avg_scores}")
 
-                self.current_block = self.node_query("System", "Number", [])
-                self.last_update = (
-                    self.current_block
-                    - self.node_query(
-                        "SubtensorModule", "LastUpdate", [self.config.netuid]
-                    )[self.my_uid]
+                self.last_update = self.subtensor.blocks_since_last_update(
+                    self.config.netuid, self.my_uid
                 )
 
-                # set weights once every tempo + 1
+                # Set weights once every tempo + 1
                 if self.last_update > self.tempo + 1:
                     total = sum(self.moving_avg_scores)
                     weights = [score / total for score in self.moving_avg_scores]
                     bt.logging.info(f"Setting weights: {weights}")
-                    # Update the incentive mechanism on the Bittensor blockchain.
-                    result = self.subtensor.set_weights(
+                    self.subtensor.set_weights(
                         netuid=self.config.netuid,
                         wallet=self.wallet,
                         uids=self.metagraph.uids,
                         weights=weights,
                         wait_for_inclusion=True,
                     )
+                    time.sleep(10) # For now to avoid too much throttling
                     self.metagraph.sync()
 
             except RuntimeError as e:
@@ -238,7 +222,6 @@ class Validator:
                 exit()
 
 
-# Run the validator.
 if __name__ == "__main__":
     validator = Validator()
     validator.run()
