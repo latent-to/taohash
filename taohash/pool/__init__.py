ce314038e41002e349391bf862cca788bfb43d11
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-03-27 17:12:21 -0700

    update __init__pool

--- taohash/pool/__init__.py
+++ taohash/pool/__init__.py
@@ -4,9 +4,6 @@ from enum import IntEnum, Enum
 import argparse
 import bittensor
 
-from .pool import PoolBase
-from .braiins import BraiinsPool
-
 
 class PoolEnum(Enum):
     Braiins = "braiins"
@@ -25,19 +22,29 @@ POOL_URLS_FMT: Dict[PoolIndex, Callable[[bittensor.AxonInfo], str]] = {
     PoolIndex.Braiins: lambda _: "stratum+tcp://stratum.braiins.com:3333",
     PoolIndex.Custom: lambda axon: f"stratum+tcp://{axon.ip}:{axon.port}"
 }
+POOL_SERVERS: Dict[PoolEnum, tuple[str, int]] = {
+    PoolEnum.Braiins: ("stratum.braiins.com", 3333),
+}
 
-
-__CLASS_MAP: Dict[PoolEnum, PoolBase] = {PoolEnum.Braiins: BraiinsPool}
+from .pool import PoolBase
+from .braiins import BraiinsPool
+_CLASS_MAP: Dict[PoolEnum, PoolBase] = {PoolEnum.Braiins: BraiinsPool}
 
 
 class Pool:
-    def __new__(cls, pool: PoolEnum, api_key: str) -> "PoolBase":
-        pool_ = __CLASS_MAP[pool]
-        api = pool_.create_api(api_key)
+    def __new__(cls, pool: PoolEnum, api_key: str, config: "argparse.Namespace") -> "PoolBase":
+        pool_ = _CLASS_MAP[pool]
+        api = pool_.create_api(api_key, config)
+        try:
+            ip, port = POOL_SERVERS[pool]
+        except KeyError:
+            raise ValueError(f"Server details not configured for pool type: {pool}")
 
-        return pool_(api_key, api)
+        return pool_(api_key, api, ip=ip, port=port)
 
     @classmethod
     def add_args(cls, parser: "argparse.ArgumentParser", _: Optional[str] = None):
-        parser.add_argument("--pool.pool", type=PoolEnum, choices=list(PoolEnum))
+        parser.add_argument("--pool.pool", required=True, type=PoolEnum, choices=list(PoolEnum))
         parser.add_argument("--pool.api_key", required=True, type=str)
+        for pool_cls in _CLASS_MAP.values():
+            pool_cls.add_args(parser)
