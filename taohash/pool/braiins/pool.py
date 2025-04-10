c725b57cb149320fe31e7b0bf526fded4d0419fa
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-04-10 12:16:09 -0700

    Update worker id for braiins

--- taohash/pool/braiins/pool.py
+++ taohash/pool/braiins/pool.py
@@ -11,6 +11,9 @@ class BraiinsPool(PoolBase):
     def __init__(self, pool_info: PoolInfo, api: BraiinsPoolAPI) -> None:
         super().__init__(pool_info, api)
 
+    def _get_worker_id_for_hotkey(self, hotkey: str) -> str:
+        return hotkey[:4] + hotkey[-4:]
+
     def get_shares_for_hotkey(self, hotkey: str, coin: str) -> float:
         worker_id = self._get_worker_id_for_hotkey(hotkey)
 
