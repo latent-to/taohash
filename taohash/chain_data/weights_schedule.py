70839f487a94bbb96ca93225b83a2a62e488a8fb
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-04-11 00:45:17 -0700

    wip: validator windows

---
+++ taohash/chain_data/weights_schedule.py
@@ -0,0 +1,55 @@
+import bittensor as bt
+from typing import Optional
+
+class WeightsSchedule:
+    """
+    Tracks block tempo to help synchronise evaluation windows.
+    """
+    
+    def __init__(
+        self,
+        subtensor: "bt.subtensor",
+        netuid: int,
+        blocks_until_eval: int = 350,
+        tempo: int = 360,
+    ):
+        """
+        Args:
+            subtensor: Bittensor subtensor instance
+            netuid: Network UID
+            blocks_until_eval: Number of blocks after which evaluation should begin (default: 350)
+            tempo: Number of blocks in an epoch (default: 360)
+        """
+        self.subtensor = subtensor
+        self.netuid = netuid
+        self.blocks_until_eval = blocks_until_eval
+        self.tempo = tempo
+
+    def blocks_until_evaluation(self) -> Optional[int]:
+        """Get number of blocks until evaluation zone starts"""
+        blocks = self.subtensor.subnet(self.netuid).blocks_since_last_step
+        return self.blocks_until_eval - blocks
+    
+    def blocks_until_next_window(self) -> Optional[int]:
+        """Get number of blocks until new tempo starts"""
+        blocks = self.subtensor.subnet(self.netuid).blocks_since_last_step
+        return self.tempo - blocks
+
+    def get_status(self) -> str:
+        """Get current status string for logging"""
+        blocks = self.subtensor.subnet(self.netuid).blocks_since_last_step
+            
+        evaluation = blocks >= self.blocks_until_eval
+        blocks_left = self.tempo - blocks if blocks else 0
+        
+        return (
+            f"Blocks since Epoch: {blocks}/{self.tempo} | "
+            f"Blocks remaining: {blocks_left} | "
+            f"In evaluation zone: {evaluation}"
+        )
+
+    # For validators
+    def should_set_weights(self) -> bool:
+        """Check if validator should set weights"""
+        blocks = self.subtensor.subnet(self.netuid).blocks_since_last_step
+        return blocks >= self.blocks_until_eval
