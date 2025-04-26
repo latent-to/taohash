4ca21f02fd8e8c319a0346f45c94e8734f99e699
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-04-25 17:57:55 -0700

    update storage

--- taohash/miner/storage.py
+++ taohash/miner/storage.py
@@ -1,13 +1,16 @@
+import json
+import base64
+
 from typing import Optional, Union
 
 from bittensor.core.config import Config
 from bittensor.utils.btlogging import logging
 
 from taohash.core.storage import BaseJsonStorage, BaseRedisStorage
+from taohash.core.storage.utils import dumps, loads
 
 
 class JsonStorage(BaseJsonStorage):
-
     def __init__(self, config: Optional["Config"] = None):
         super().__init__(config)
 
@@ -24,14 +27,20 @@ class JsonStorage(BaseJsonStorage):
         return self.get_latest()
 
     def save_schedule(self, block_number: int, schedule_obj) -> None:
-        self.save_data(key=block_number, data=schedule_obj, prefix="schedule")
+        readable_data = json.dumps(schedule_obj, default=lambda x: x.__dict__)
+        dumped_data = dumps(schedule_obj)
+        string_data = base64.b64encode(dumped_data).decode("utf-8")
+        dict_data = {"data": readable_data, "data_encoded": string_data}
+        self.save_data(key=block_number, data=dict_data, prefix="schedule")
 
     def load_latest_schedule(self):
-        return self.get_latest("schedule")
+        data = self.get_latest("schedule")
+        string_data = data.get("data_encoded")
+        decoded_data = base64.b64decode(string_data.encode("utf-8"))
+        return loads(decoded_data)
 
 
 class RedisStorage(BaseRedisStorage):
-
     def __init__(self, config: Optional["Config"] = None):
         super().__init__(config)
 
@@ -56,7 +65,9 @@ class RedisStorage(BaseRedisStorage):
 STORAGE_CLASSES = {"json": JsonStorage, "redis": RedisStorage}
 
 
-def get_miner_storage(storage_type: str, config: "Config") -> Union[JsonStorage, RedisStorage]:
+def get_miner_storage(
+    storage_type: str, config: "Config"
+) -> Union[JsonStorage, RedisStorage]:
     """Get storage instance based on type."""
     if storage_type not in STORAGE_CLASSES:
         raise ValueError(f"Unknown storage type: {storage_type}")
