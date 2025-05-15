8573710483a128a7b3f39d754717cf0244ffb424
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-15 02:14:16 -0700

    change to debug

--- taohash/miner/proxy/taohash_proxy/src/logger.py
+++ taohash/miner/proxy/taohash_proxy/src/logger.py
@@ -2,7 +2,7 @@ import logging
 
 if not logging.getLogger().handlers:
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
