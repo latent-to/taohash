72a28e5b620aabb5ce5a20b4e29a29c4764bf143
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-27 03:50:23 -0700

    init validator proxy

---
+++ proxy/__init__.py
@@ -0,0 +1,6 @@
+"""
+TaoHash Proxy - Validator proxy for TaoHash.
+
+This package provides the core implementation of the TaoHash mining proxy, 
+which serves as an intermediary between miners and mining pools.
+"""
