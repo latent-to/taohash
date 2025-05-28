21dbd4712c5842996001a711f6a3f556a334ef32
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-27 19:37:57 -0700

    moves metrics + add proxy metrics handler

---
+++ taohash/core/pool/metrics/__init__.py
@@ -0,0 +1,15 @@
+"""
+Pool metrics module for tracking miner performance.
+"""
+
+from .base import BaseMetrics
+from .braiins import BraiinsMetrics, get_metrics_for_miners
+from .proxy import ProxyMetrics, get_metrics_timerange
+
+__all__ = [
+    "BaseMetrics",
+    "BraiinsMetrics", 
+    "ProxyMetrics",
+    "get_metrics_for_miners",
+    "get_metrics_timerange",
+]
