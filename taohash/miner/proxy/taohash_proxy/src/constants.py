e5eb82f998ccc72dd4ee8c77577216d3b44703cf
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-27 19:39:44 -0700

    wip

--- taohash/miner/proxy/taohash_proxy/src/constants.py
+++ taohash/miner/proxy/taohash_proxy/src/constants.py
@@ -3,16 +3,16 @@ Shared constants for the TaoHash proxy.
 """
 
 # Used by controller to load new config in the running proxy
-RELOAD_API_PORT = 5001  
+RELOAD_API_PORT = 5010  
 RELOAD_API_HOST = "0.0.0.0"
 
 # Used by miners to connect to the proxy
-INTERNAL_PROXY_PORT = 3331  
-DEFAULT_PROXY_PORT = 3331  
+INTERNAL_PROXY_PORT = 3333  
+DEFAULT_PROXY_PORT = 3333  
 
 # Used by the controller to view the proxy stats
-INTERNAL_DASHBOARD_PORT = 5000  
-DEFAULT_DASHBOARD_PORT = 5000  
+INTERNAL_DASHBOARD_PORT = 8100  
+DEFAULT_DASHBOARD_PORT = 8100  
 
 # Default path to the config file
 CONFIG_PATH = "config/config.toml"  
