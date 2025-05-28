06fca3ce05f7980f0994a2a172ac86a657917fb9
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-28 01:22:35 -0700

    removes vali proxy

--- proxy/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-TaoHash Proxy - Validator proxy for TaoHash.
-
-This package provides the core implementation of the TaoHash mining proxy, 
-which serves as an intermediary between miners and mining pools.
-"""
