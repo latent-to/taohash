4d3ec84316970a10109d57803c3e0f1b5cf4c2ed
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-05-30 18:23:42 -0700

    update envs, tomls

--- taohash/miner/proxy/taohash_proxy/src/logger.py
+++ taohash/miner/proxy/taohash_proxy/src/logger.py
@@ -2,7 +2,7 @@ import logging
 
 if not logging.getLogger().handlers:
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
