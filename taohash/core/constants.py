1cb3185a641de9ba7cec5c975873b9b1a794d0f1
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-06-23 12:35:15 -0700

    updates version key

--- taohash/core/constants.py
+++ taohash/core/constants.py
@@ -4,5 +4,5 @@ BLOCK_TIME = 12  # Seconds per block
 
 MAIN_PATH = Path("~", ".bittensor", "taohash").expanduser()
 
-VERSION_KEY = 26  # For validators
+VERSION_KEY = 27  # For validators
 U16_MAX = 65535
