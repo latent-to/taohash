061f37c50608223ce0713e0a5e8cf300f9a78d49
ibraheem-latent <i@latent.to>
2025-10-01 15:15:08 -0700

    bumps version

--- taohash/core/constants.py
+++ taohash/core/constants.py
@@ -4,7 +4,7 @@ BLOCK_TIME = 12  # Seconds per block
 
 MAIN_PATH = Path("~", ".bittensor", "taohash").expanduser()
 
-VERSION_KEY = 32  # For validators
+VERSION_KEY = 33  # For validators
 U16_MAX = 65535
 
 OWNER_TAKE = 0.18
