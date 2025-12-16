58fd93fed86c4540eda2e6e7775b8c507edcb5b4
ibraheem-latent <i@latent.to>
2025-12-15 19:42:40 -0800

    update proxy api to use hyphens

--- taohash/core/pool/proxy/api.py
+++ taohash/core/pool/proxy/api.py
@@ -42,10 +42,8 @@ class ProxyPoolAPI(PoolAPI):
     @staticmethod
     def _worker_name_to_worker_id(worker_name: str) -> str:
         splits = worker_name.split(".")
-        if len(splits) == 1:  # no period
-            return splits[0]
-        else:
-            return splits[-1][:8]  # Take 8 chars of HK slug after period
+        hotkey_part = splits[-1] if len(splits) > 1 else splits[0]
+        return hotkey_part.split("-")[0]  # Strip worker ID suffix if present
 
     @on_exception(
         expo,
